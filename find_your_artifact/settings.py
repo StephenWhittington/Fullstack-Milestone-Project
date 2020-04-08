d6dd8629e8e63b1731c95696998f7ae49a5bbed1
StephenWhittington <stephen_whittington@hotmail.co.uk>
2020-04-08 17:23:31 +0000

    added settings to send users an email to reset their password

--- find_your_artifact/settings.py
+++ find_your_artifact/settings.py
@@ -128,6 +128,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
