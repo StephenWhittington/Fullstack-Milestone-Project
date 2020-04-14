78820e57380bd08151e818420baf39f72f840cb7
StephenWhittington <stephen_whittington@hotmail.co.uk>
2020-04-14 14:50:28 +0000

    trying to get email backend working

--- find_your_artifact/settings.py
+++ find_your_artifact/settings.py
@@ -168,6 +168,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
