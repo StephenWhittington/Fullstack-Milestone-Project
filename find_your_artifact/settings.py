d2cf9d25952979ed344b52beb8435fa8f987092c
StephenWhittington <stephen_whittington@hotmail.co.uk>
2020-04-14 13:44:31 +0000

    added custom media storage background image now works on heroku

--- find_your_artifact/settings.py
+++ find_your_artifact/settings.py
@@ -149,6 +149,13 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
