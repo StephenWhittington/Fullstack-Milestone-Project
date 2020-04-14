055650e089db6e4094866d1923f83e2b3e52bfa1
StephenWhittington <stephen_whittington@hotmail.co.uk>
2020-04-14 15:47:10 +0000

    reverted back

--- find_your_artifact/settings.py
+++ find_your_artifact/settings.py
@@ -164,7 +164,7 @@ AUTHENTICATION_BACKENDS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = [os.environ.get('EMAIL_ADDRESS')]
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
