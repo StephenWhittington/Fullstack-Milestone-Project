22759c1ce483ec43ddb889ee6a8d3db561f399b7
StephenWhittington <stephen_whittington@hotmail.co.uk>
2020-04-19 19:36:32 +0000

    fixed typo and now can view checkout form with styling

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,7 +7,7 @@ class ProcessPaymentForm(forms.Form):
     YEAR_CHOICES = [(i, i) for i in range(2019, 2040)]
 
     credit_card_number = forms.CharField(label='Credit card number', required=False)
-    cvv = forms.CharField(label='Security code (CW', required=False)
+    cvv = forms.CharField(label='Security code (CVV)', required=False)
     expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.ChoiceField(widget=forms.HiddenInput)
