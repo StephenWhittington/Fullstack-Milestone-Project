d42857b22844b5d6c3ce9aa43366d62862ffb49c
StephenWhittington <stephen_whittington@hotmail.co.uk>
2020-04-19 13:12:25 +0000

    search bar now renders artifacts page

--- search/views.py
+++ search/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from artifacts.models import Artifact
 
 def do_search(request):
-    artifacts = rtifacts.objects.filter(name__icontains=request.GET['q'])
+    artifacts = Artifact.objects.filter(name__icontains=request.GET['q'])
     return render(request, "artifacts.html", {"artifacts": artifacts})
