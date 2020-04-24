241f73a3b1506bb1e3c116a39fccd970cc573a8e
StephenWhittington <stephen_whittington@hotmail.co.uk>
2020-04-24 14:42:45 +0000

    added a model to my accounts app to store more user info, removed community link going to add messages into artifacts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    """
+
+    This model will contain all of a customers key information, apart from what is required for
+    authentication (username, password and email). The fields are:
+    - description (a text field the user can fill in giving personal or other information, to
+    describe to other users what they are looking for)
+    - address
+    We also of course use a OneToOneField to link it to a specific user!
+    
+    """
+    user = models.OneToOneField(User)
+    full_name = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    street_address1 = models.CharField(max_length=40, blank=False)
+    street_address2 = models.CharField(max_length=40, blank=False)
+    county = models.CharField(max_length=40, blank=False)
+
+    def __unicode__(self):
+        return self.user.username
